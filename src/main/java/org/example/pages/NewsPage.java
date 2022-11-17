49523f28d69f04357b69744bd9f82263c382a9b0
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-17 10:47:59 +0300

    make NewsPage

---
+++ src/main/java/org/example/pages/NewsPage.java
@@ -0,0 +1,34 @@
+package org.example.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import org.jetbrains.annotations.NotNull;
+
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+
+public class NewsPage
+{
+
+  @NotNull
+  private final SelenideElement SEARCH_FIELD = $(byName("st.query"));
+  @NotNull
+  private final SelenideElement USER =
+    $(byXpath("//div[contains(@class, 'card-caption__a0i64 card-caption-small__02cy5') and " +
+      "text() = \"technoPol17 technoPol17\"]"));
+
+  private final SelenideElement FRIENDS_SECTION = $(byXpath("//div[@data-l='t,userFriend']"));
+
+  public UserPage findUser(@NotNull final String name)
+  {
+    SEARCH_FIELD.setValue(name);
+    USER.click();
+    return new UserPage();
+  }
+
+  public FriendsPage goToFriends()
+  {
+    FRIENDS_SECTION.click();
+    return new FriendsPage();
+  }
+}
